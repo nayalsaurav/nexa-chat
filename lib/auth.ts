7841666f9fa22c025ae303832a21d7f6b36f7fd1
Saurav Nayal <nayalsaurav@gmail.com>
2026-02-15 18:55:08 +0530

    feat(db): setup database schema and drizzle configuration

---
+++ lib/auth.ts
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import GitHub from "next-auth/providers/github";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "@/database";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(db),
+
+  providers: [Google, GitHub],
+
+  pages: {
+    signIn: "/signin",
+  },
+
+  session: {
+    strategy: "database",
+  },
+
+  secret: process.env.AUTH_SECRET,
+});
